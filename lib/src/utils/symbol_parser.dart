11b6f09cd3709224d40fc3cdde5da61f04c172bb
伍枭鹏 <1109695647@qq.com>
2021-06-26 20:21:51 +0800

    fix：升级 flutter sdk，null safe

--- lib/src/utils/symbol_parser.dart
+++ lib/src/utils/symbol_parser.dart
@@ -1,6 +1,9 @@
 RegExp replaceSymbolRegExp = new RegExp('Symbol|\\("|"\\)');
 
 /// 解析 Symbol的name
-String parseSymbolName(Symbol symbol) {
+String? parseSymbolName(Symbol? symbol) {
+  if (symbol == null) {
+    return null;
+  }
   return symbol.toString().replaceAll(replaceSymbolRegExp, "");
 }
